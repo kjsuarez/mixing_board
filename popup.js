5905fc39e2d243fc07733fc93443eb882c372557
kjsuarez <kjsuarez94@gmail.com>
2021-07-24 10:12:17 -0700

    baseline mvp established

--- popup.js
+++ popup.js
@@ -8,6 +8,37 @@ let setVolumeButton = document.getElementById("setVolumeButton");
 // When the button is clicked, inject setPageBackgroundColor into current page
 findButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      {
+        greeting: "hello"
+      },
+      function(response) {
+        console.log("response of find audios button");
+        console.log(response);
+        let container = document.getElementById("sliders");
+        console.log(container)
+        while (container.firstChild) {
+          container.removeChild(container.firstChild);
+        }
+        let audios = response.audio_element_data;
+        for(var i = 0; i < audios.length; i++){
+          console.log("what extension thinks the volume of " + audios[i].element_id + " is: " + audios[i].volume)
+          let original = document.getElementById("baseline_volume_slider");
+          let slider_clone = original.cloneNode(true);
+          slider_clone.style.removeProperty('display');
+          slider_clone.classList.add("volume_slider");
+          slider_clone.dataset["relatedId"] = audios[i].element_id
+          slider_clone.value = String(audios[i].volume * 100)
+          console.log("set slider to " + slider_clone.value)
+          container.append(slider_clone)
+        }
+      }
+    );
+  });
+
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: addSliders,
@@ -39,10 +70,25 @@ setVolumeButton.addEventListener("click", async () => {
 
 function addSliders() {
   var audios = document.getElementsByTagName('audio');
-  for(var i = 0; i < audios.length; i++){
-    console.log(audios[i].id)
-    //audios[i].volume = 0.1
-  }
+  var audios_array = [].slice.call(audios);
+
+  let formatted_audio_array = audios_array.map(audio => {
+    console.log("current audio volume: " + audio.volume)
+     let rObj = {}
+     rObj["element_id"] = audio.id;
+     rObj["volume"] =  audio.volume;
+     return rObj
+  })
+
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log("triggered in addSliders");
+
+      sendResponse({audio_element_data: formatted_audio_array});
+    }
+  );
+
+
 }
 
 // The body of this function will be executed as a content script inside the
@@ -50,18 +96,16 @@ function addSliders() {
 function setVolume(sliders) {
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.greeting === "hello")
-        console.log(request)
-        for (var i = 0; i < request.audio_changes.length; i++) {
-          // array element example: {element_id: "jp_audio_0", volume: 0.2}
-          var el = document.getElementById(request.audio_changes[i].element_id)
-          console.log("element id: " + request.audio_changes[i].element_id);
-          el.volume = request.audio_changes[i].volume;
-        }
-        sendResponse({farewell: "goodbye"});
+
+      console.log(request)
+      for (var i = 0; i < request.audio_changes.length; i++) {
+        // array element example: {element_id: "jp_audio_0", volume: 0.2}
+        var el = document.getElementById(request.audio_changes[i].element_id)
+        console.log("element id: " + request.audio_changes[i].element_id);
+        el.volume = request.audio_changes[i].volume;
+        console.log("volume of element after setting: " + el.volume)
+      }
+      sendResponse({farewell: "goodbye"});
     }
   );
 }
