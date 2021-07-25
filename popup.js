bea8c5d9e0c6a9afe67ed3af04be58697f840584
kjsuarez <kjsuarez94@gmail.com>
2021-07-25 00:23:43 -0700

    clean up popup ui

--- popup.js
+++ popup.js
@@ -1,12 +1,7 @@
 let findButton = document.getElementById("findElementsButton");
-let setVolumeButton = document.getElementById("setVolumeButton");
 
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-findButton.addEventListener("click", async () => {
+window.onload = async () => {
+  console.log("onload" + Date())
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -24,17 +19,50 @@ findButton.addEventListener("click", async () => {
           container.removeChild(container.firstChild);
         }
         let audios = response.audio_element_data;
-        for(var i = 0; i < audios.length; i++){
-          console.log("what extension thinks the volume of " + audios[i].element_id + " is: " + audios[i].volume)
-          let original = document.getElementById("baseline_volume_slider");
-          let slider_clone = original.cloneNode(true);
-          slider_clone.style.removeProperty('display');
-          slider_clone.classList.add("volume_slider");
-          slider_clone.dataset["relatedId"] = audios[i].element_id
-          slider_clone.value = String(audios[i].volume * 100)
-          console.log("set slider to " + slider_clone.value)
-          container.append(slider_clone)
+        if (audios.length > 0) {
+          for(var i = 0; i < audios.length; i++){
+            console.log("what extension thinks the volume of " + audios[i].element_id + " is: " + audios[i].volume)
+            let original_label = document.getElementById("baseline_label");
+            let label_clone = original_label.cloneNode(true);
+            label_clone.style.removeProperty('display');
+            label_clone.innerText = audios[i].element_id
+
+            let original = document.getElementById("baseline_volume_slider");
+            let slider_clone = original.cloneNode(true);
+            slider_clone.style.removeProperty('display');
+            slider_clone.classList.add("volume_slider");
+            slider_clone.dataset["relatedId"] = audios[i].element_id
+            slider_clone.value = String(audios[i].volume * 100)
+            console.log("set slider to " + slider_clone.value)
+            container.append(label_clone)
+            container.append(slider_clone)
+            slider_clone.addEventListener('change', async () => {
+              let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+              let volume_info = get_volume_info();
+              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(
+                  tabs[0].id,
+                  {
+                    greeting: "hello",
+                    audio_changes: volume_info
+                  },
+                  function(response) {
+                    console.log(response.farewell);
+                  }
+                );
+              });
+
+              chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                function: setVolume,
+              });
+            });
+          }
+        } else {
+          let audio_missing_message = document.getElementById("no_audio_message");
+          audio_missing_message.style.removeProperty('display');
         }
+
       }
     );
   });
@@ -43,30 +71,91 @@ findButton.addEventListener("click", async () => {
     target: { tabId: tab.id },
     function: addSliders,
   });
-});
+}
 
-setVolumeButton.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  let volume_info = get_volume_info();
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(
-      tabs[0].id,
-      {
-        greeting: "hello",
-        audio_changes: volume_info
-      },
-      function(response) {
-        console.log(response.farewell);
-      }
-    );
-  });
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) {
+    console.log("This Works");
+    // do your things
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setVolume,
-  });
-});
+    // chrome.tabs.sendMessage(
+    //   tabId,
+    //   {
+    //     greeting: "hello"
+    //   },
+    //   function(response) {
+    //     console.log("response of find audios button");
+    //     console.log(response);
+    //     let container = document.getElementById("sliders");
+    //     console.log(container)
+    //     while (container.firstChild) {
+    //       container.removeChild(container.firstChild);
+    //     }
+    //     let audios = response.audio_element_data;
+    //     for(var i = 0; i < audios.length; i++){
+    //       console.log("what extension thinks the volume of " + audios[i].element_id + " is: " + audios[i].volume)
+    //       let original = document.getElementById("baseline_volume_slider");
+    //       let slider_clone = original.cloneNode(true);
+    //       slider_clone.style.removeProperty('display');
+    //       slider_clone.classList.add("volume_slider");
+    //       slider_clone.dataset["relatedId"] = audios[i].element_id
+    //       slider_clone.value = String(audios[i].volume * 100)
+    //       console.log("set slider to " + slider_clone.value)
+    //       container.append(slider_clone)
+    //     }
+    //   }
+    // );
+    //
+    //
+    // chrome.scripting.executeScript({
+    //   target: { tabId: tab.id },
+    //   function: addSliders,
+    // });
+  }
+})
 
+
+
+// chrome.tabs.onUpdated.addListener(async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.sendMessage(
+//       tabs[0].id,
+//       {
+//         greeting: "hello"
+//       },
+//       function(response) {
+//         console.log("response of find audios button");
+//         console.log(response);
+//         let container = document.getElementById("sliders");
+//         console.log(container)
+//         while (container.firstChild) {
+//           container.removeChild(container.firstChild);
+//         }
+//         let audios = response.audio_element_data;
+//         for(var i = 0; i < audios.length; i++){
+//           console.log("what extension thinks the volume of " + audios[i].element_id + " is: " + audios[i].volume)
+//           let original = document.getElementById("baseline_volume_slider");
+//           let slider_clone = original.cloneNode(true);
+//           slider_clone.style.removeProperty('display');
+//           slider_clone.classList.add("volume_slider");
+//           slider_clone.dataset["relatedId"] = audios[i].element_id
+//           slider_clone.value = String(audios[i].volume * 100)
+//           console.log("set slider to " + slider_clone.value)
+//           container.append(slider_clone)
+//         }
+//       }
+//     );
+//   });
+//
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: addSliders,
+//   });
+// });
+
+// When the button is clicked, inject setPageBackgroundColor into current page
 
 function addSliders() {
   var audios = document.getElementsByTagName('audio');
